0adf6403f75cf32a92a56414eaba66e1d2079135
PasDeBras <paul.ponnau@gmail.com>
2019-01-23 18:08:43 +0100

    added single post view

--- index.php
+++ index.php
@@ -2,7 +2,38 @@
 require('controller/frontend.php');
 
 try { // On essaie de faire des choses
-    listPosts();
+    if (isset($_GET['action'])) {
+        if ($_GET['action'] == 'listPosts') {
+            listPosts();
+        }
+        elseif ($_GET['action'] == 'post') {
+            if (isset($_GET['id']) && $_GET['id'] > 0) {
+                post();
+            }
+            else {
+                // Erreur donc exception, envoyée au catch
+                throw new Exception('Aucun identifiant de billet envoyé');
+            }
+        }
+        elseif ($_GET['action'] == 'addComment') {
+            if (isset($_GET['id']) && $_GET['id'] > 0) {
+                if (!empty($_POST['author']) && !empty($_POST['comment'])) {
+                    addComment($_GET['id'], $_POST['author'], $_POST['comment']);
+                }
+                else {
+                    // Autre erreur
+                    throw new Exception('Erreur : tous les champs ne sont pas remplis !');
+                }
+            }
+            else {
+                // Autre erreur
+                throw new Exception('Erreur : aucun identifiant de billet envoyé');
+            }
+        }
+    }
+    else {
+        listPosts();
+    }
 }
 catch(Exception $e) {
     $errorMessage = $e->getMessage();
