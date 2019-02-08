a2c2d77dcbfc4cbea133f5d9c2169aa591f585db
PasDeBras <paul.ponnau@gmail.com>
2019-02-08 14:59:55 +0100

    change comment moderation view to table

--- view/backend/commentModeratorView.php
+++ view/backend/commentModeratorView.php
@@ -3,17 +3,31 @@
 <?php ob_start(); ?>
 <h1>Commentaires en attente de modération</h1>
 <p>Ces commentaires ont été signalés par vos lecteurs, ils sont classés du plus grand nombre de signalements au plus petit.</p>
-<?php
-        while ($comment = $comments->fetch())
-        {
+<table style="width:100%">
+    <tr>
+        <th>ID</th>
+        <th>Auteur</th> 
+        <th>Commentaire</th>
+        <th>Nbre de signalements</th>
+        <th>Suppression</th>
+        <th>Deflag</th>
+    </tr>
+    <?php 
+    while ($comment = $comments->fetch())
+    {
         ?>
-            <p>Utilisateur : <strong><?= htmlspecialchars($comment['author']) ?></strong> à posté le <?= $comment['comment_date_fr'] ?> :</p>
-            <p><?= nl2br(htmlspecialchars($comment['comment'])) ?></p>
-            <p>Ce commentaire à été signalé <?= $comment['flagged'] ?> fois,</p>
-            <p><a href="index.php?action=deleteComment&amp;commentId=<?= $comment['id'] ?>">supprimer ce commentaire</a> ou <a href="index.php?action=unflagComment&amp;commentId=<?= $comment['id'] ?>">unflag ce commentaire</a></p>
+        <tr>
+            <th><?= $comment['id'] ?></th>
+            <th><?= htmlspecialchars($comment['author']) ?></th> 
+            <th><?= nl2br(htmlspecialchars($comment['comment'])) ?></th>
+            <th><?= $comment['flagged'] ?></th>
+            <th><a href="index.php?action=deleteComment&amp;commentId=<?= $comment['id'] ?>">supprimer ce commentaire</a></th>
+            <th><a href="index.php?action=unflagComment&amp;commentId=<?= $comment['id'] ?>">unflag ce commentaire</a></th>
+        </tr>
         <?php
-        }
-        ?>
+    }
+    ?>
+</table>        
         
 
 <?php $content = ob_get_clean(); ?>
