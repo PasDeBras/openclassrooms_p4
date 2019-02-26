8e9d43cf541a2b46e4d14eaba89d66dfe4c4512b
PasDeBras <paul.ponnau@gmail.com>
2019-02-27 00:43:51 +0100

    added support for small screen on comment moderator view

--- view/backend/commentModeratorView.php
+++ view/backend/commentModeratorView.php
@@ -5,33 +5,35 @@
 
 <h1 id="commentModerator_Title">Commentaires en attente de modération</h1>
 <p id="commentModerator_Notice">Ces commentaires ont été signalés par vos lecteurs, ils sont classés du plus grand nombre de signalements au plus petit.</p>
-<table id="commentModerator_Table" style="width:100%">
-    <tr id="commentModerator_Table_Labels">
-        <th>POST</th>    
-        <th>ID</th>
-        <th>AUTEUR</th> 
-        <th>COMMENTAIRE</th>
-        <th>SIGNALEMENTS</th>
-        <th>SUPPRESSION</th>
-        <th>VALIDATION</th>
-    </tr>
-    <?php 
-    while ($comment = $comments->fetch())
-    {
-        ?>
-        <tr class="commentModerator_Table_Items">
-            <td class="commentModerator_Table_Items_Post"><a href="index.php?action=adminPostView&amp;id=<?= $comment['post_id'] ?>" title="Se rendre sur la page du chapitre concerné">Chapitre</a></th>
-            <td class="commentModerator_Table_Items_Id"><?= $comment['id'] ?></th>
-            <td class="commentModerator_Table_Items_Author"><?= htmlspecialchars($comment['author']) ?></th> 
-            <td class="commentModerator_Table_Items_Comment"><?= nl2br(htmlspecialchars($comment['comment'])) ?></th>
-            <td class="commentModerator_Table_Items_Flagged"><?= $comment['flagged'] ?></th>
-            <td class="commentModerator_Table_Items_Delete"><a href="index.php?action=deleteComment&amp;commentId=<?= $comment['id'] ?>" title="Supprimer ce commentaire">Supprimer</a></th>
-            <td class="commentModerator_Table_Items_UnFlag"><a href="index.php?action=unflagComment&amp;commentId=<?= $comment['id'] ?>" title="Valider ce commentaire">Valider</a></th>
-        </tr>
-        <?php
-    }
-    ?>
-</table>
+<div id="table_container">
+    <table id="commentModerator_Table" style="width:100%">
+            <tr id="commentModerator_Table_Labels">
+                <th>POST</th>    
+                <th>ID</th>
+                <th>AUTEUR</th> 
+                <th>COMMENTAIRE</th>
+                <th>SIGNALEMENTS</th>
+                <th>SUPPRESSION</th>
+                <th>VALIDATION</th>
+            </tr>
+            <?php 
+            while ($comment = $comments->fetch())
+            {
+                ?>
+                <tr class="commentModerator_Table_Items">
+                    <td class="commentModerator_Table_Items_Post"><a href="index.php?action=adminPostView&amp;id=<?= $comment['post_id'] ?>" title="Se rendre sur la page du chapitre concerné">Chapitre</a></th>
+                    <td class="commentModerator_Table_Items_Id"><?= $comment['id'] ?></th>
+                    <td class="commentModerator_Table_Items_Author"><?= htmlspecialchars($comment['author']) ?></th> 
+                    <td class="commentModerator_Table_Items_Comment"><?= nl2br(htmlspecialchars($comment['comment'])) ?></th>
+                    <td class="commentModerator_Table_Items_Flagged"><?= $comment['flagged'] ?></th>
+                    <td class="commentModerator_Table_Items_Delete"><a href="index.php?action=deleteComment&amp;commentId=<?= $comment['id'] ?>" title="Supprimer ce commentaire">Supprimer</a></th>
+                    <td class="commentModerator_Table_Items_UnFlag"><a href="index.php?action=unflagComment&amp;commentId=<?= $comment['id'] ?>" title="Valider ce commentaire">Valider</a></th>
+                </tr>
+                <?php
+            }
+            ?>
+    </table>
+</div>
 <div id="commentModerator_help">
     <h2>Aide :</h2>    
     <p>
@@ -43,7 +45,7 @@
         <b>Suppression :</b> Suppression definitive du commentaire.<br>
         <b>Validation :</b> Remise à zero du nombre de signalement pour ce commentaire.
     </p>
-</div>     
+</div>    
         
 <?php $content = ob_get_clean(); ?>
 
